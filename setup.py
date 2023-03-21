46aece3b81d19c0739a737ccb26b8103b66f41eb
Pranav Tondgaonkar <pranavrelds@gmail.com>
2023-03-22 04:20:58 +0530

    inital setup complete

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NLP-NEWS-SUMMERIZATION",
+    version="0.0.1",
+    author="Pranav Tondgaonkar",
+    author_email="pranavrelds@gmail.com",
+    packages=find_packages(),
+)
